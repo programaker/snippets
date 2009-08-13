2fbe478fff7f7af973326ef991db091b895cdce9
programaker <programaker@gmail.com>
2009-08-13 14:05:13 -0300

    how to list all constraints of a table in oracle

---
+++ sql/oracle-list-all-constraints.sql
@@ -0,0 +1,5 @@
+select * from all_constraints where table_name = '<table_name>'
+
+-- where constraint_type = 'P' -> primary key
+--                       = 'R' -> foreign keys
+--                       = 'C' -> constraints like not null
