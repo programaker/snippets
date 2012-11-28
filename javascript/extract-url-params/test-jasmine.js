bf0a3318a6be68ed018b65a1611a5ff48b3047b1
programaker <programaker@gmail.com>
2012-11-28 15:58:20 -0200

    adding jasmine tests alongside qunit tests

---
+++ javascript/extract-url-params/test-jasmine.js
@@ -0,0 +1,25 @@
+describe('Url without parameters', function() {
+  var url = new Url('http://www.youtube.com/');
+
+  it('should return "undefined" when trying to get a param value by name', function() {
+    var v = url.param('v');
+    expect(v).toBe(undefined);
+
+    var feature = url.param('feature');
+    expect(feature).toBe(undefined);
+  });
+});
+
+describe('Url with the parameters "v" and "feature"', function() {
+  var url = new Url('http://www.youtube.com/watch?v=JLGRkYYmJgQ&feature=g-vrec');
+
+  it('should get the value of param "v" by name', function() {
+    var v = url.param('v');
+    expect(v).toEqual('JLGRkYYmJgQ');
+  });
+
+  it('should get the value of param "feature" by name', function() {
+    var feature = url.param('feature');
+    expect(feature).toBe('g-vrec');
+  });
+});
